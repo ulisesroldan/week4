8cec078e2f3e1a9c16eb1dd18a25d072579a57b0
Francisco Ulises Roldan Trejo <UlisesRoldan@macbook-pro-de-francisco.local>
2016-06-12 22:21:18 -0500

    Initial Commit

---
+++ Semana4Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Semana4Swift
+//
+//  Created by Francisco Ulises Roldan Trejo on 12/06/16.
+//  Copyright © 2016 Francisco Ulises Roldan Trejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
